58fc9e52ed5d7a73bc54f3bf35648d8701fb7d00
thiagopls1 <thiagopls1@hotmail.com>
2022-11-04 12:53:59 -0300

    setted up SignIn, Loading and Theme

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/Loading";
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello react native!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsLoaded ? <SignIn /> : <Loading /> }
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 12
-  }
-});
