6167ce71cf12a193e558b26de37f9e07a37868d7
Thiago Pereira Lins <thiagopls1@hotmail.com>
2022-11-07 11:56:54 -0300

    added new screens

---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,32 @@
+import { Heading, VStack, Text } from 'native-base';
+import { Input } from '../components/Input';
+import { Header } from '../components/Header';
+import { Button } from '../components/Button';
+import Logo from '../assets/logo.svg';
+
+
+export function New(){
+  return(
+    <VStack flex={1} bgColor="gray.900">
+      <Header title="Criar novo bolão" showBackButton/>
+      <VStack mt={8} mx={5} alignItems="center">
+        <Logo/>
+        <Heading fontFamily="heading" color="white" fontSize="xl" my={8}>
+          Crie seu próprio bolão da copa {'\n'} e compartilhe com amigos!
+        </Heading>
+        <Input 
+          mb={2}
+          placeholder="Qual o nome do seu bolão?"
+        />
+
+        <Button
+          title="CRIAR MEU BOLÃO"
+        />
+        <Text color="gray.200" fontSize="sm" textAlign="center" px={10} mt={4}>
+          Após criar o seu bolão, você receberá um código único
+          que poderá usar para convidar outras pessoas.
+        </Text>
+      </VStack>
+    </VStack>
+  );
+}
